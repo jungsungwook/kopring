94f75b7f16ce2d08be2c79b330543595ce2143f8
jungsungwook <jswcyber@naver.com>
2023-08-03 11:55:45 +0900

    init: project

---
+++ src/test/kotlin/com/jungsungwook/myspring/MyspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jungsungwook.myspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyspringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
