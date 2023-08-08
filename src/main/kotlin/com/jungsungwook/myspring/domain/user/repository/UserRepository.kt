7e981479d491e41bc8bdf8095328274d7ab69b55
jungsungwook <jswcyber@naver.com>
2023-08-08 16:25:31 +0900

    feat: user repository

---
+++ src/main/kotlin/com/jungsungwook/myspring/domain/user/repository/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.jungsungwook.myspring.domain.user.repository
+import com.jungsungwook.myspring.domain.user.models.entity.User
+import org.springframework.data.repository.CrudRepository
+interface UserRepository : CrudRepository<User, String>
