1ccff86e07861f5a7a1608351b2054a23e187d0d
jungsungwook <jswcyber@naver.com>
2023-08-08 16:25:41 +0900

    add: user entity

--- src/main/kotlin/com/jungsungwook/myspring/domain/user/models/entity/User.kt
+++ src/main/kotlin/com/jungsungwook/myspring/domain/user/models/entity/User.kt
@@ -4,16 +4,22 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "user")
-class User(name: String, email: String){
+class User(name: String, email: String, customId: String, password: String){
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
+
+    @Column(name = "custom_id", nullable = false)
+    var customId: String = customId
 
     @Column(name = "name", nullable = false)
     var name: String = name
 
     @Column(name = "email", nullable = false)
     var email: String = email
+
+    @Column(name = "password", nullable = false)
+    var password: String = password
 
     @Column(name = "created_at", nullable = false)
     var createdAt: LocalDateTime? = null
