f69f6ed3e346567d76a85a6a1f936e49e56fa9fc
jungsungwook <jswcyber@naver.com>
2023-08-09 17:14:05 +0900

    feat: signin service

--- src/main/kotlin/com/jungsungwook/myspring/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/jungsungwook/myspring/domain/auth/service/AuthService.kt
@@ -1,14 +1,20 @@
 package com.jungsungwook.myspring.domain.auth.service
+import com.jungsungwook.myspring.domain.auth.models.dto.SignInDto
 import com.jungsungwook.myspring.domain.auth.models.dto.SignUpDto
 import com.jungsungwook.myspring.domain.user.models.entity.User
 import com.jungsungwook.myspring.domain.user.repository.UserRepository
+import com.jungsungwook.myspring.global.utils.TokenProvider
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class AuthService(
         private val userRepository: UserRepository,
-        private val encoder: BCryptPasswordEncoder
+        private val encoder: BCryptPasswordEncoder,
+        private val tokenProvider: TokenProvider
 ) {
     fun signUpByPost(dto: SignUpDto) {
         var user: User = User(
@@ -18,5 +24,13 @@ class AuthService(
                 name = dto.name,
         )
         userRepository.save(user)
+    }
+
+    fun signInByPost(dto: SignInDto): String {
+        var user: User? = userRepository.findByCustomId(dto.customId)
+                ?.takeIf { encoder.matches(dto.password, it.password) }
+                ?: throw IllegalArgumentException("아이디 또는 비밀번호가 일치하지 않습니다.")
+        var token: String = tokenProvider.createToken(dto.customId)
+        return token
     }
 }
