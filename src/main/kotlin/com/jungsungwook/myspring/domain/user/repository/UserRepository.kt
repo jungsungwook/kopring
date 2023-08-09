030d07160a57141853f85a957e8c136f6fcda99c
jungsungwook <jswcyber@naver.com>
2023-08-09 17:14:37 +0900

    feat: repository

--- src/main/kotlin/com/jungsungwook/myspring/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/jungsungwook/myspring/domain/user/repository/UserRepository.kt
@@ -1,4 +1,6 @@
 package com.jungsungwook.myspring.domain.user.repository
 import com.jungsungwook.myspring.domain.user.models.entity.User
 import org.springframework.data.repository.CrudRepository
-interface UserRepository : CrudRepository<User, String>
+interface UserRepository : CrudRepository<User, String> {
+    fun findByCustomId(customId: String): User?
+}
