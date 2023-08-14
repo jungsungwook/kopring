ae771a752777adba1aad5e93464d84b1f7403f36
jungsungwook <jswcyber@naver.com>
2023-08-14 09:36:34 +0900

    feat: custom userdetail

---
+++ src/main/kotlin/com/jungsungwook/myspring/global/security/CustomUserDetails.kt
@@ -0,0 +1,25 @@
+package com.jungsungwook.myspring.global.security
+
+import com.jungsungwook.myspring.domain.user.models.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.UserDetails
+
+class CustomUserDetails(val user: User) : UserDetails {
+
+    var enabled: Boolean = true
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = AuthorityUtils.createAuthorityList()
+
+    override fun getPassword(): String = user.password
+
+    override fun getUsername(): String = user.password
+
+    override fun isAccountNonExpired(): Boolean = enabled
+
+    override fun isAccountNonLocked(): Boolean = enabled
+
+    override fun isCredentialsNonExpired(): Boolean = enabled
+
+    override fun isEnabled(): Boolean = enabled
+}
