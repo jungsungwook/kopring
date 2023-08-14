7a79aaf3ad0c1c02ca3ffd69611e277df0769a49
jungsungwook <jswcyber@naver.com>
2023-08-14 09:35:58 +0900

    fix: dto separate

--- src/main/kotlin/com/jungsungwook/myspring/domain/auth/models/dto/SignUpDto.kt
+++ src/main/kotlin/com/jungsungwook/myspring/domain/auth/models/dto/SignUpDto.kt
@@ -1,8 +1,13 @@
 package com.jungsungwook.myspring.domain.auth.models.dto
 
-data class SignUpDto(
+data class SignUpRequestDto(
         val customId: String,
         val password: String,
         val name: String,
         val email: String
+)
+
+data class SignUpResponseDto(
+        val statusCode: String,
+        val content: String,
 )
